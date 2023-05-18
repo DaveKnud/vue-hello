7249ca4d14051bf7dddcff26d6aae83343ab19db
DaveKnud <daveknud@outlook.com>
2023-05-18 22:35:28 +0200

    correction

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const {createText} = Vue;
+const {createApp} = Vue;
 
-createText({
+createApp({
     data() {
         return {
-            randomText: "Amigo", 
-            
+            randomText: "Amigo",
+            randomNumbers :"13,15,16,24",
+            image: "https://fastly.picsum.photos/id/878/300/300.jpg?hmac=nEnxwevmM_w7wyX2VIqEKx6oeuSC5Ybq-zy1xM5e_eo"
         }
     }
 }) .mount("#myText");
