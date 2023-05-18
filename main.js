4965a856dd4d6054e7dd8df02a88fd9497d90d99
DaveKnud <daveknud@outlook.com>
2023-05-18 22:54:49 +0200

    aggiunta immagine

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ createApp({
         return {
             randomText: "Amigo",
             randomNumbers :"13,15,16,24",
-            image: "https://fastly.picsum.photos/id/878/300/300.jpg?hmac=nEnxwevmM_w7wyX2VIqEKx6oeuSC5Ybq-zy1xM5e_eo"
+            image: "https://avatars.githubusercontent.com/u/109771606?s=48&v=4"
         }
-    }
+    },
+    
 }) .mount("#myText");
